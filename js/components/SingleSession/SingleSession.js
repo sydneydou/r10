437f47acc0a7abe16ff8479861e5b8ccba4e88e5
Sydney <sydney@Hings-MBP.hitronhub.home>
2019-08-21 19:18:40 -0700

    Added styles to single session page

--- js/components/SingleSession/SingleSession.js
+++ js/components/SingleSession/SingleSession.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View,Button} from 'react-native';
+import {Text, View,Button,Image} from 'react-native';
 import styles from './styles';
 
 const SingleSession = ({singleSession}) => {
@@ -7,17 +7,23 @@ const SingleSession = ({singleSession}) => {
         hour: '2-digit',
         minute: '2-digit',
       })
+
+      const imageurl = singleSession.speaker.url
   return (
-    <View>
+    <View style={styles.sessionPage}>
       
-      <Text>{singleSession.location}</Text>
-      <Text>{singleSession.title}</Text>
-      <Text>{time}</Text>
-      <Text>{singleSession.description}</Text>
-      <View>
-          <Text>Presented By:</Text>
-          {/* image here */}
-          <Text>{singleSession.speaker.name}</Text>
+      <Text style={styles.sessionLocation}>{singleSession.location}</Text>
+      <Text style={styles.sessionTitle}>{singleSession.title}</Text>
+      <Text style={styles.sessionTime}>{time}</Text>
+      <Text style={styles.sessionDescription}>{singleSession.description}</Text>
+      
+          <Text style={styles.sessionLocation}>Presented By:</Text>
+          <View> 
+          <Image
+          style={{width: 50, height: 50}}
+          source={{uri: {imageurl}}}
+        />
+          <Text style={styles.sessionSpeakerName} >{singleSession.speaker.name}</Text>
       </View>
 
 
